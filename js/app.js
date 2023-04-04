a3da85475222274f9d9f6d16f4c39ffa02a946c3
dogukankaynar <dogukankaynar98@gmail.com>
2023-04-04 16:30:42 +0300

    js dosyaları modüler hale getirilip storage.js eklendi

---
+++ js/app.js
@@ -0,0 +1,27 @@
+import {memberRegistration} from "./storage.js"
+let subButton = document.getElementById("subButton");
+
+let data = JSON.parse(localStorage.getItem("myData"));
+
+data.map(data=>{console.log(data)})
+
+
+// let time = new Date(data[0].sonGün.getTime())
+/*   ==========>datadan gelen date' ye göre kalan günleri hesaplar<========
+for(let i in data){
+ let time = new Date(data[i].sonGün);
+ let newTime = time.getTime();
+ console.log(time);
+ console.log("yeni tarih", newTime);
+
+let now = new Date()
+ let remainingTime = newTime - now.getTime();
+ let  remainingDays = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+  console.log(data[i].name+" Üyesinin Kalan Günü: "+remainingDays)
+}*/
+
+function eventListener() {
+  subButton.addEventListener("click", memberRegistration);//üye kayıtını başlatır.
+}
+
+eventListener();
---
+++ js/storage.js
@@ -0,0 +1,59 @@
+let personName = document.getElementById("personName");
+let personSurname = document.getElementById("personSurname");
+let person = [];
+
+const startStroge = () => {
+  const data = localStorage.getItem("myData");
+  if (!data) {
+    localStorage.setItem("myData", JSON.stringify([]));
+  }
+  console.log("çalıstım");
+};
+startStroge();
+
+// localStorage' e üye Ekler
+const addStorage = (person) => {
+  const data = JSON.parse(localStorage.getItem("myData"));
+  data.push(person);
+  localStorage.setItem("myData", JSON.stringify(data));
+};
+
+//üyelik ay süresini hesaplar
+const calculatePeriod = () => {
+  let selectPeriod = document.getElementById("period");
+  let selectValue = parseInt(
+    selectPeriod.options[selectPeriod.selectedIndex].value
+  );
+  return selectValue;
+};
+
+//üye kayıt fonksiyonu
+const memberRegistration = () => {
+  let registrationDate = new Date(); //kayıt tarihi
+  let id = registrationDate.getTime(); //üye id
+
+  let value = calculatePeriod(); //üyenin aylık süresi
+  console.log(value);
+
+  //üyelik bitiş tarihi
+  let endDate = new Date(
+    registrationDate.getFullYear(),
+    registrationDate.getMonth() + value,
+    registrationDate.getDate()
+  );
+
+  //üye objesi
+  person = {
+    id: id,
+    name: personName.value,
+    surname: personSurname.value,
+    kayıtTarihi: registrationDate,
+    sonGün: endDate,
+  };
+
+  addStorage(person);
+
+  console.log("storage js çalıstı");
+};
+
+export  {memberRegistration}
